fc5bf84125c50438a6026b65727d17e63af068c5
e2318501 <e2318501@outlook.com>
2023-11-20 09:38:25 +0900

    feat: limit item transactions between inventories

--- ItemTransactionLimiter/src/main/java/net/nutchi/itemtransactionlimiter/listener/PlayerListener.java
+++ ItemTransactionLimiter/src/main/java/net/nutchi/itemtransactionlimiter/listener/PlayerListener.java
@@ -1,14 +1,14 @@
-package net.nutchi.dropbroker.listener;
+package net.nutchi.itemtransactionlimiter.listener;
 
 import lombok.RequiredArgsConstructor;
-import net.nutchi.dropbroker.DropBroker;
+import net.nutchi.itemtransactionlimiter.ItemTransactionLimiter;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 
 @RequiredArgsConstructor
 public class PlayerListener implements Listener {
-    private final DropBroker plugin;
+    private final ItemTransactionLimiter plugin;
 
     @EventHandler
     public void onPlayerDrop(PlayerDropItemEvent event) {
